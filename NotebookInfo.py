61387b2102c11a2926b078570f8020b061c40081
DmitriyKochurov <dimon7932rtek@gmail.com>
2022-06-08 15:07:31 +0300

    Notebook Initialization. 3-rd stage.

--- NotebookInfo.py
+++ NotebookInfo.py
@@ -10,8 +10,73 @@ class NotebookInfo(object):
         self.Memory = memory
         self.Storage = storage
 
+
+    def un_notebook(self):
+        print(f"""Ноутбук производства {self.Manufacturer} {self.Model} с характеристиками: 
+                    *Диагональ экрана: {self.Screensize}
+                    *Процессор: {self.CPU}
+                    *Кол-во оперативной памяти: {self.Memory}
+                    *Кол-во памяти : {self.Storage} 
+относится к бюджетной или бизнес линейки ноутбуков""")
+
+
+    def notebook(self, line, segment):
+        print(f"""Ноутбук производства {self.Manufacturer} {line} {self.Model} относится к {segment} серии с характеристиками: 
+            *Диагональ экрана: {self.Screensize}
+            *Процессор: {self.CPU}
+            *Кол-во оперативной памяти: {self.Memory}
+            *Кол-во памяти : {self.Storage} """)
+
+
     def get_info(self):
-        pass
+        line = str
+        segment = str
+        if len(str(self.Model).split()) == 1:
+            if self.Manufacturer == "Asus":
+                for i in asus.keys():
+                    if str(self.Model).upper() in i:
+                        line = asus.get(i)
+                        break
+                if line == str:
+                    line = ""
+            elif self.Manufacturer == "Lenovo":
+                for i in lenovo.keys():
+                    if str(self.Model).upper() in i:
+                        line = lenovo.get(i)
+                        break
+                    if line == str:
+                        line = ""
+            elif self.Manufacturer == "HP":
+                for i in hp.keys():
+                    if str(self.Model).upper() in i:
+                        line = hp.get(i)
+                        break
+                    if line == str:
+                        line = ""
+            elif self.Manufacturer == "Razer":
+                for i in razer.keys():
+                    if str(self.Model).upper() in i:
+                        line = razer.get(i)
+                        break
+                    if line == str:
+                        line = ""
+            else:
+                line = ""
+        elif len(str(self.Model).split()) == 2:
+            line, self.Model = str(self.Model).split()
+        else:
+            line = ""
+        if line != "":
+            if line[-1] == "7":
+                segment = "игровой"
+            elif line[-1] == "5":
+                segment = "мультимедийный" or "бизнес"
+            else:
+                segment = "бюджетный"
+        if line == "":
+            self.un_notebook()
+        else:
+            self.notebook(line, segment)
 
 
 if __name__ == "__main__":
--- NotebookSpecification.py
+++ NotebookSpecification.py
@@ -1,8 +1,11 @@
 asus = {("ZenBook Pro", "ZenBook OLED 13") : "Core i7",
         ("ROG Strix G17", "VivoBook Pro 15", "ROG Zephyrus M16", "ROG Strix G15") : "Ryzen 7" }
+
 lenovo = {("Legion 5", "Legion 5 Pro") : "Core i5" ,
                   ("IdeaPad 5", "IdeaPad 5 Pro") : "Ryzen 5",
                   ("ThinkPad") : "Core i3"}
+
 hp = {("Envy 13", "Pavilion Gaming") : "Core i7",
               ("Victus 16", "OMEN 15", "ProBook 455") : "Ryzen 7"}
+
 razer = {("Blade Stealth 13", "Blade 17", "Book 13") : "Core i7"}
